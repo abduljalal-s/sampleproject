123f59b82f8c2de6bcf1ed49692bdc86e692b3e9
abduljalal-s <nightmadecoder@gmail.com>
2025-03-07 02:48:01 +0100

    seciring the app cache.ts

--- MoonLight/app/_layout.tsx
+++ MoonLight/app/_layout.tsx
@@ -1,14 +1,30 @@
-    import { Stack } from "expo-router";
+    import { ClerkLoaded, ClerkProvider } from '@clerk/clerk-expo';
+import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+const tokenCache = {}; // Define tokenCache
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+  )
+}
 
     export default function RootLayout() {
       return(
-        <SafeAreaProvider>
+
+        <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+
+     
+        <SafeAreaProvider> 
         <SafeAreaView style={{flex: 1, backgroundColor:"black"}}>
 
         <Stack screenOptions={{headerShown: false}} />
         </SafeAreaView> 
         </SafeAreaProvider>
-
+ </ClerkLoaded>  
+ </ClerkProvider>
           );
         }
